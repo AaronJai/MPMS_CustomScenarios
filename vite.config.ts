09c7c910aebfce94b0a5c896041fc0d1be08ee48
Aaron <aaron.tan1@gmail.com>
2025-09-08 14:08:50 +0800

    shadcn + tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
+import tailwindcss from "@tailwindcss/vite"
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react(), tailwindcss(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
@@ -26,4 +27,9 @@ export default defineConfig({
         : {},
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
